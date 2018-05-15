a84138edb08e0c505f1bea770f0bd173d32a91b2
Maxwell <mxt.developer@gmail.com>
2018-05-15 23:13:58 +0200

    Moved __init__.py
    Moved main execution entry point into project root directory

--- __init__.py
+++ __init__.py
@@ -1,19 +1,14 @@
-import os
 import sys
 from app.presenter import BasePresenter, PersistencePresenter
 
 
 def init_app(filename=None):
-    
     base_presenter = BasePresenter(filename)
     base_presenter.create_resources()
     
-    print()
-    
     persistence_presenter = PersistencePresenter(base_presenter)
     persistence_presenter.create_directories()
-    
-    print()
+
     print("-----------------------------------")
     print("End of execution")
     print("-----------------------------------")
@@ -21,9 +16,11 @@ def init_app(filename=None):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        if os.path.isfile(sys.argv[1]):
+        if sys.argv[1].__contains__('.json'):
             init_app(sys.argv[1])
         else:
             print(sys.argv[1] + " is not a valid file.")
     else:
-        print("Usage: app.py [filename]")
+        print("All files must be placed inside the .app/io/input directory before hand.")
+        print("Usage: python3 __init__.py filename.json")
+        print("e.g. python3 __init__.py workspace_2018-04-29.json")
